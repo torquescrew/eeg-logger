07366bd2e13e6207c9836eaf3024cb61ef365aea
Toby <sugto555@gmail.com>
2015-12-26 21:48:11 +1300

    Delete old files.

--- client/test2.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-export let sqr2 = (n: number) => n * n;
-
-
-export default function sqr(n: number) {
-    return n * n;
-}
