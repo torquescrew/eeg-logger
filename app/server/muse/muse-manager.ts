9ce3caf0494f1a211b15e7ca8fc9896b96337ee6
Toby Suggate <t.suggate@kuracloud.com>
2016-12-09 16:58:46 +1300

    Add muse manager.

---
+++ app/server/muse/muse-manager.ts
@@ -0,0 +1,26 @@
+import {HeadsetManager} from "../headset";
+
+
+export class MuseManager implements HeadsetManager {
+
+   connect() {
+
+   }
+
+   isConnected(): boolean {
+      return false;
+   }
+
+   startRecording(): void {
+
+   }
+
+   stopRecording(): void {
+
+   }
+
+   getRecordedData(): any[] {
+      return [];
+   }
+
+}
